47d9ae372770b685286ac9b35c368e2a80cf4ff0
akhfa <akhmadfakhoni@gmail.com>
2015-02-21 14:10:03 +0700

    Libary for writing matriks

---
+++ matriks.hpp
@@ -0,0 +1,53 @@
+#ifndef MATRIKS_H
+#define MATRIKS_H
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+class Matriks
+{
+	private:
+		int size;
+		ofstream myfile;
+		string matriks;
+		int randNumber;
+		
+	public:
+		Matriks(int _N)
+		{
+			size	= _N;
+			
+		}
+		void setMatriksSize(int _N)
+		{
+			size	= _N;
+		}
+		
+		int getMatriksSize()
+		{
+			return size;
+		}
+		
+		void writeMatriks()
+		{
+			myfile.open ("matriks.txt");
+			srand((unsigned)time(0));
+			for(int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					randNumber = rand() % 19 + (-9);
+					if(j == 0)
+						myfile << randNumber;
+					else
+						myfile << " " << randNumber;
+				}
+				myfile << "\n";
+			}
+			myfile.close();
+		}
+};
+#endif
