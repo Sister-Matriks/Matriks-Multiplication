72080f9fcf6770a693179b9546faca7bfb9cc4db
akhfa <akhmadfakhoni@gmail.com>
2015-02-22 13:48:27 +0700

    Added matriks.hpp again. Welcome back :D

---
+++ matriks.hpp
@@ -0,0 +1,75 @@
+#ifndef MATRIKS_H
+#define MATRIKS_H
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+class Matriks
+{
+	private:
+		int size;
+		ofstream myfile;
+		string matriks;
+		int randNumber;
+		
+	public:
+		Matriks()
+		{
+			size = 4;
+		}
+		
+		Matriks(int _N)
+		{
+			size	= _N;
+			
+		}
+		void setSize(int _N)
+		{
+			size	= _N;
+		}
+		
+		int getSize()
+		{
+			return size;
+		}
+		
+		void writeMatriks()
+		{
+			myfile.open ("matriks.txt");
+			srand((unsigned)time(0));
+			for(int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					randNumber = rand() % 19 + (-9);
+					if(j == 0)
+						myfile << randNumber;
+					else
+						myfile << " " << randNumber;
+				}
+				myfile << "\n";
+			}
+			myfile.close();
+		}
+		
+		void writeMatriks(int value)
+		{
+			myfile.open ("matriks.txt");
+			for(int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					if(j == 0)
+						myfile << value;
+					else
+						myfile << " " << value;
+				}
+				myfile << "\n";
+			}
+			myfile.close();
+		}
+};
+#endif
